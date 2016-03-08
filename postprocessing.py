a82f2dc3b7e402b9060a001773297fefdc3f7f42
Tram Anh <tramanh06@gmail.com>
2016-03-08 11:10:44 +0800

    postprocessing is useless. Merged in test

--- postprocessing.py
+++ postprocessing.py
@@ -2,27 +2,6 @@ __author__ = 'TramAnh'
 
 import csv
 
-
-def decode(x):
-    if x > 0.75:
-        return 'G'
-    elif x>0.5:
-        return 'C'
-    elif x>0.25:
-        return 'T'
-    else:
-        return 'A'
-
-def similarity(a, b):
-    # assert(len(a)==len(b))
-    # print 'length a=%d' %(len(a))
-    # print 'length b=%d' %(len(b))
-    score = 0
-    for i in range(len(a)):
-        if a[i]==b[i]:
-            score += 1
-    return float(score)/len(a)
-
 def postproc_fn():
     f = open('predictions.txt', 'rb')
     p_sequences = []
@@ -34,14 +13,20 @@ def postproc_fn():
         p_sequences.append(temp)
 
     # Compare with test.csv
+    # TODO: fix index out of bound error
+    # elaborate: predictions.txt only includes 20 sequences
+    # while test.csv has a lot more than 20 sequences
+    # Reason: test.csv can have a lot of data, but only 20 are valid (that only contains ATCG)
     similar = []
     with open('test.csv', 'rb') as testfile:
         reader = csv.reader(testfile,)
         for i, row in enumerate(reader):
-            print i
             d = row[1]
             o = p_sequences[i]
             similar.append(similarity(d, o))
 
     mean = sum(similar)/len(similar)
     print 'Mean similarity= %f' %(mean)
+
+if '__name__'=='__main__':
+    postproc_fn()
