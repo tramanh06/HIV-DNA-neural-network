cadb27cb46c4b93f75b8439a1f3e360cdfd0f92a
Tram Anh <tramanh06@gmail.com>
2016-03-09 10:04:09 +0800

    script to split data according to drug

--- split_drug_script.py
+++ split_drug_script.py
@@ -1,2 +1,18 @@
 __author__ = 'TramAnh'
 
+import csv
+import pandas as pd
+
+infile = 'Data/alldata.csv'
+
+# Read csv
+alldata_df = pd.read_csv(infile, sep="\t")
+
+# get unique drug name
+unique_drug = alldata_df.Drug_D.unique()
+
+# dictionary of drug as key and DF as value
+DF_dict = { elem: pd.DataFrame for elem in unique_drug}
+
+for key in DF_dict.keys():
+    DF_dict[key] = alldata_df[:][alldata_df.Drug_D==key]
