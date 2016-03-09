f3a892cc8182db5befc9a7ec739d777e95d4421d
Tram Anh <tramanh06@gmail.com>
2016-03-09 09:26:41 +0800

    Refactor to test any given csv file

--- MLP_script.py
+++ MLP_script.py
@@ -4,19 +4,34 @@ import train
 import test
 import numpy as np
 from multiprocessing import Pool
-
+import matplotlib.pyplot as plt
 score=[]
+testfile = 'train_cleaned.csv'
+
+def plot_histogram(r, hiddennodes):
+    plt.subplot(240+hiddennodes-2)
+    plt.hist(r, bins=20)
+    plt.title("hiddennodes=%s" %hiddennodes)
+    plt.ylim(0,25)
 
 def mapper(hiddennodes):
     print 'Running hiddennode=%d' %(hiddennodes)
     # train.train_fn(hiddennodes)
-    r_score = test.test_fn(hiddennodes)
-    # score.append([hiddennodes, score_test])
+    r_score = test.test_fn(testfile, hiddennodes)
+    # score.append([hiddennodes, score_test])R
+    print 'Length of samples= %s'%len(r_score)
     print 'Hidden nodes= {0}    Score_test= {1}'.format(hiddennodes, r_score)
     return hiddennodes, r_score
 
+plt.figure(1)
 pool = Pool(8)
 out= pool.map(mapper, range(3,11))
+
+for i in out:
+    nodes = i[0]
+    plot_histogram(i[1], nodes)
+
+plt.show()
 
 print 'out= %s' %(out)
 score.append(out)
--- test.py
+++ test.py
@@ -10,18 +10,17 @@ from math import sqrt
 import matplotlib.pyplot as plt
 
 MAXLENGTH = 297
-test_file = 'test.csv'
 
 output_predictions_file = 'predictions.txt'
 
-def test_fn(hiddennodes):
+def test_fn(testfile, hiddennodes):
     # load model
     model_file = 'model_{0}_nodes.pkl'.format(str(hiddennodes))
     net = pickle.load( open( model_file, 'rb' ))
     print 'Finish loading model'
 
     # Load test data
-    x_test, y_test = load_data('test.csv')
+    x_test, y_test = load_data(testfile)
     y_test_dummy = np.zeros( y_test.shape )
     input_size = x_test.shape[1]
     target_size = y_test.shape[1]
@@ -39,7 +38,6 @@ def test_fn(hiddennodes):
     p = net.activateOnDataset( ds )
 
     r_score = calculate_correlation(y_test, p)
-    plot_histogram(r_score, hiddennodes)
     return r_score
 
 def calculate_correlation(y_test, p):
@@ -49,72 +47,6 @@ def calculate_correlation(y_test, p):
         r.append(r_score[0])        # pearsonr returns correlation and 2-tailed p-value. Only need correlation
     return r
 
-def plot_histogram(r, hiddennodes):
-    plt.hist(r, bins=20)
-    plt.title("Corellation Histogram for hiddennodes=%s" %hiddennodes)
-    plt.xlabel("R value")
-    plt.ylabel("Frequency")
-
-    plt.show()
-
-def compare_ytest_p(y_test, p):
-    # Calculate similarity (in number) between y_test and p
-    score = 0
-
-    for i, row in enumerate(p):
-        p_decoded = []      # p_decoded is to store decoded value (decode to 0.25, 0.5,..., 1)
-        for each in row:
-            decoded_x = decode(each)
-            p_decoded.append(decoded_x)
-        score_temp =  similarity(p_decoded, y_test[i])
-        if (score_temp <0.80):
-            print 'Score below 0.80'
-            print 'Score= {0}'.format(score_temp)
-            temp_ytest = y_test[i]
-            # plot_low_score(y_test[i], row, temp)
-        score += score_temp
-
-    average_score = score / len(p)
-
-    print 'Average score = {0}'.format(average_score)
-    return average_score
-
-def plot_low_score(desired, predicted, p_encode):
-    x_axis = range(1, MAXLENGTH+1)
-    plt.figure(1)
-    # plt.subplot(211)
-    plt.plot(x_axis, desired, 'bo', x_axis, p_encode, 'ro')
-    plt.show()
-
-# TODO: determine threshold so it can tolerate fluctuation
-def decode(x):
-    if x > 0.85:
-        return 1
-    elif x>0.65:
-        return 0.75
-    elif x>0.35:
-        return 0.5
-    elif x>0.15:
-        return 0.25
-    else:
-        return 0
-
-def similarity(a, b):
-    '''
-    Calculate similarity between 2 strings
-    :param a: array containing encoded sequences
-    :param b: array containing encoded sequences
-    :return: score of similarity between the 2 array sequences
-    '''
-    assert(len(a)==len(b))
-
-    # print 'length a=%d' %(len(a))
-    # print 'length b=%d' %(len(b))
-    score = 0
-    for i in range(len(a)):
-        if a[i]==b[i]:
-            score += 1
-    return float(score)/len(a)
 
 
 if __name__=='__main__':
