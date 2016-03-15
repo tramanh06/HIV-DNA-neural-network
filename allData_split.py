7e187a8fd7b74708a9cb9e1fd343665c512433f6
Tram Anh <tramanh06@gmail.com>
2016-03-15 13:33:29 +0800

    Refactor variable

--- allData_split.py
+++ allData_split.py
@@ -2,7 +2,12 @@ __author__ = 'TramAnh'
 
 ''' Split to train and test '''
 
+
 from filter_input_data import filter, write_csv
+
+# def filter_short_seq(data):
+#     for each in data:
+#
 
 filename = 'Data/trainvaltest.csv'
 
@@ -10,16 +15,17 @@ unseen_ratio = 0.15
 test_ratio = 0.15
 
 ' Filter out noise sequence in trainvaltest.csv'
-trainvaltest = filter(filename)
+trainvaltest_filtered = filter(filename)        # trainvaltest_filtered = [[inp, out], [inp,out],...]
+
 
 ' Split data '
-num_sample = len(trainvaltest)
+num_sample = len(trainvaltest_filtered)
 traintest_mark = int((1-test_ratio-unseen_ratio)*num_sample)
 testunseen_mark = int((1-unseen_ratio)*num_sample)
 
-train = trainvaltest[:traintest_mark]
-test = trainvaltest[traintest_mark:testunseen_mark]
-unseen = trainvaltest[testunseen_mark:]
+train = trainvaltest_filtered[:traintest_mark]
+test = trainvaltest_filtered[traintest_mark:testunseen_mark]
+unseen = trainvaltest_filtered[testunseen_mark:]
 
 'Write to csv'
 write_csv('Data/train_cleaned.csv', train)
