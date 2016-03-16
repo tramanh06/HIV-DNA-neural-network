0d9e3b75b71b0c43a1a96f1b98facf6c3b3f8b14
Tram Anh <tramanh06@gmail.com>
2016-03-16 12:37:03 +0800

    Convert traintest file to fasta for alignment
    Add converst_4fasta.py to convert train and test csv file
    to fasta format (>1W, >1M,..)

---
+++ convert_traintest_2fasta.py
@@ -0,0 +1,30 @@
+__author__ = 'TramAnh'
+
+import csv
+
+def get_sequences(filename, arr):
+    with open(filename, 'rb') as f:
+        csvreader = csv.reader(f)
+        for line in csvreader:
+            arr.append(line)
+
+def write_fasta(outfile, arr):
+    with open(outfile, 'wb') as f:
+        for i, line in enumerate(arr):
+            f.write('>{0}W\n'.format(i))
+            f.write(line[0].strip()+'\n')
+            f.write('>{0}M\n'.format(i))
+            f.write(line[1].strip()+'\n')
+
+trainfile = 'Data/train_cleaned.csv'
+testfile = 'Data/test_cleaned.csv'
+outfile = 'Data/fasta_4alignment.txt'
+
+arr=[]
+get_sequences(trainfile, arr)
+get_sequences(testfile, arr)
+
+write_fasta(outfile, arr)
+
+
+
