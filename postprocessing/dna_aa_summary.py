61dfecd0c34415a6dcb138ec9673da12beceff2f
Tram Anh <tramanh06@gmail.com>
2016-03-30 17:14:03 +0800

    Finish summary for test data
    SUmmary includes:
    - dna and amino acid for wildtype, mutant, predicted
    - confusion matrix for each sequence
    - levenshtein distance for (mutant, predicted)

--- postprocessing/dna_aa_summary.py
+++ postprocessing/dna_aa_summary.py
@@ -1,8 +1,11 @@
-from utils import dna_to_aa
+from utils import dna_to_aa, confusion_matrix
 import csv
+from Levenshtein import distance
 
-predictfile = '../results_log/prediction.csv'       # in DNA
-actualfile = '../Data/alignment/test_aligned.csv'   # WT, MT in DNA
+predictfile = '/Users/TramAnh/Dropbox/NTU/BII_Parttime_work/HIV/ForSight Results/3rd_set/set3_converted.txt'       # in DNA
+actualfile = '/Users/TramAnh/Dropbox/NTU/BII_Parttime_work/HIV/ForSight Results/test_aligned.csv'   # WT, MT in DNA
+outfile = '/Users/TramAnh/Dropbox/NTU/BII_Parttime_work/HIV/ForSight Results/3rd_set/summary.csv'
+
 
 def alert(has_stop):
     if has_stop:
@@ -16,14 +19,26 @@ if __name__== '__main__':
     actual_mt_dna = []
     actual_mt_aa = []
 
+    stats_dna = []
+    stats_aa = []
+
+    dist_dna = []
+    dist_aa = []
+
+    CODON_MSG = 'Contains stop codon'
+
+    # Analyse prediction
     with open(predictfile, 'rb') as f:
         for line in f:
             dna = line.strip()
             predict_dna.append(dna)
             has_stop, aa = dna_to_aa(dna)     # dna_to_aa returns [has_stop_codon, aa]
             alert(has_stop)
+            if has_stop:
+                aa = CODON_MSG
             predict_aa.append(aa)
 
+    # Analyse clinical data
     with open(actualfile, 'rb') as f:
         csvreader = csv.reader(f)
         for line in csvreader:
@@ -35,22 +50,41 @@ if __name__== '__main__':
             # Convert to Amino Acid
             has_stop, wt_aa = dna_to_aa(wt)
             alert(has_stop)
+            if has_stop:
+                wt_aa = CODON_MSG
             actual_wt_aa.append(wt_aa)
 
             has_stop, mt_aa = dna_to_aa(mt)
             alert(has_stop)
+            if has_stop:
+                mt_aa = CODON_MSG
             actual_mt_aa.append(mt_aa)
 
-    with open('summary.csv', 'wb') as f:
+    # Calculate confusion matrix
+    for i in range(len(actual_wt_dna)):
+        stats_dna.append(confusion_matrix(wt=actual_wt_dna[i],
+                                          mt=actual_mt_dna[i],
+                                          predicted=predict_dna[i]).__str__())
+        stats_aa.append(confusion_matrix(wt=actual_wt_aa[i],
+                                         mt=actual_mt_aa[i],
+                                         predicted=predict_aa[i]).__str__())
+
+        dist_dna.append(distance(actual_mt_dna[i].lower(), predict_dna[i].lower()))
+        dist_aa.append(distance(actual_mt_aa[i].lower(), predict_aa[i].lower()))
+
+    # Calculate Levenstein distance
+
+
+    with open(outfile, 'wb') as f:
         csvwriter = csv.writer(f)
-        headers = ['Wildtype (DNA)', 'Mutant (DNA)', 'Predicted (DNA)',
-                   'Wildtype (AA)', 'Mutant (AA)', 'Predicted (AA)']
+        headers = ['Wildtype (DNA)', 'Mutant (DNA)', 'Predicted (DNA)', 'ConfMatrix (DNA)', 'Distance (DNA)',
+                   'Wildtype (AA)', 'Mutant (AA)', 'Predicted (AA)', 'ConfMatrix (AA)', 'Distance (AA)']
         csvwriter.writerow(headers)
 
         num_seq = len(predict_dna)
         for i in range(num_seq):
-            row = [actual_wt_dna[i], actual_mt_dna[i], predict_dna[i],
-                   actual_wt_aa[i], actual_mt_aa[i], predict_aa[i]]
+            row = [actual_wt_dna[i], actual_mt_dna[i], predict_dna[i], stats_dna[i], dist_dna[i],
+                   actual_wt_aa[i], actual_mt_aa[i], predict_aa[i], stats_aa[i], dist_aa[i]]
             csvwriter.writerow(row)
 
 
--- postprocessing/utils.py
+++ postprocessing/utils.py
@@ -2,6 +2,7 @@ __author__ = 'TramAnh'
 
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_rna
+import numpy as np
 
 def dna_to_aa(sequence):
     ''' Convert DNA to Amino Acid sequence.
@@ -11,12 +12,54 @@ def dna_to_aa(sequence):
         print len(sequence)
 
     sample = Seq(sequence, generic_rna)
-    amino_acid = sample.translate(stop_symbol='-')
+    amino_acid = sample.translate(to_stop=True).__str__()
 
-
-    codon = amino_acid.
+    codon = False
     if len(amino_acid) < 225/3:
         'Stop codon detected'
         codon = True
 
     return codon, amino_acid
+
+def confusion_matrix(wt, mt, predicted):
+    '''
+    Take in wildtype, mutant and predicted sequence.
+    String type.
+    Compute a confusion matrix of
+                predicted
+                NC          C
+    actual  NC
+            C
+    '''
+
+    if 'stop codon' in predicted:
+        return
+
+    # Make sure same length
+    assert(len(wt) == len(mt))
+    assert(len(mt) == len(predicted))
+
+    tp, fp_nc, fp_c, tn, fn = [0] * 5
+
+    for i in range(len(wt)):
+        w = wt[i].lower()
+        m = mt[i].lower()
+        p = predicted[i].lower()
+
+        if m == w:
+            if p == w:
+                tp += 1
+            else:
+                fn += 1
+        else:   # case m != w
+            if p == m:  # change correctly
+                tn += 1
+            elif p == w:
+                fp_nc += 1
+            else:
+                fp_c += 1
+
+    matrix = [[tp, fn], [[fp_nc, fp_c], tn]]
+
+    return matrix
+
